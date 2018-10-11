79a573b6423f91c1d6d830d45191833bc00c6c51
Gil <gilfpf@hotmail.com>
2018-10-11 09:09:35 +0100

    complete

---
+++ models/traveller.js
@@ -0,0 +1,60 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+
+  return this.journeys.map(journey =>{
+    return journey.startLocation
+  });
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+
+  return this.journeys.map(journey =>{
+    return journey.endLocation
+
+  });
+};
+
+Traveller.prototype.getModesOfTransport = function () {
+
+  return this.journeys.map(journey => {
+    return journey.transport
+
+  });
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+
+  return this.journeys.filter((journey) =>{
+    return journey.transport === transport;
+
+  });
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+
+  return this.journeys.filter((journey) =>{
+    return journey.distance >= minDistance;
+
+  });
+
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function (totalDistance) {
+
+  return this.journeys.reduce((total, journey) =>{
+    return total += journey.distance;
+
+  },0);
+};
+
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+
+
+};
+
+
+module.exports = Traveller;
